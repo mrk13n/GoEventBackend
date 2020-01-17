5b0f04c9e1b7677fd6e50924c1492fd0bd9a429e
Nazar <naz466@yandex.ru>
2020-01-17 20:09:11 +0200

    delete https

--- Backend/main.js
+++ Backend/main.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const https = require('https');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
 const api = require('./api');
 
 const configureEndpoints = (app) => {
@@ -23,13 +21,7 @@ const startServer = (port) => {
 
     configureEndpoints(app);
 
-    const privateKey  = fs.readFileSync(__dirname + '/../selfsigned.key', 'utf8');
-    const certificate = fs.readFileSync(__dirname + '/../selfsigned.crt', 'utf8');
-    const credentials = { key: privateKey, cert: certificate };
-
-    const httpsServer = https.createServer(credentials, app);
-
-    httpsServer.listen(port, () => {
+    app.listen(port, () => {
         console.log('My Application Running on http://localhost:'+port+'/');
     });
 };
