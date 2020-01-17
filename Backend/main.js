6b303de319932b908b89bc3d5671c486fd8fdb21
Nazar <naz466@yandex.ru>
2020-01-17 12:20:50 +0200

    Initial commit

---
+++ Backend/main.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const api = require('./api');
+
+const configureEndpoints = (app) => {
+
+};
+
+const startServer = (port) => {
+    const app = express();
+
+    app.use(morgan('dev'));
+
+    app.use(bodyParser.urlencoded());
+    app.use(bodyParser.json());
+
+    app.use(cors());
+
+    configureEndpoints(app);
+
+    app.listen(port, () => {
+        console.log('My Application Running on http://localhost:'+port+'/');
+    });
+};
+
+exports.startServer = startServer;
